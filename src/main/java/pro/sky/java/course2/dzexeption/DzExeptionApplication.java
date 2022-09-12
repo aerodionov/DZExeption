48b5ed5f14c4e97bbba4d739a950b991cb382539
aerod <aerodionov@mail.ru>
2022-09-12 20:43:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/dzexeption/DzExeptionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.dzexeption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzExeptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzExeptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/dzexeption/DzExeptionApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.dzexeption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzExeptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
